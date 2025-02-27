f4f978b5e25a1881a037c409f0214026b3c1a5b2
Rudy Gutierrez <93001008+rudy-gm@users.noreply.github.com>
2025-02-27 01:22:32 -0500

    Lesson 2.2 - Food Flight

--- Projects/Prototype2/Assets/Scripts/PlayerController.cs
+++ Projects/Prototype2/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10; 
+    public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,7 +30,16 @@ public class PlayerController : MonoBehaviour
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch Projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+        }
+
+
     }
 
 
