0a1b0a0a49150f7a521a2eaeb31ed03c78805107
Rudy Gutierrez <93001008+rudy-gm@users.noreply.github.com>
2025-02-27 02:20:21 -0500

    Lesson 2.4 - Collision Decisions

--- Projects/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Projects/Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -20,6 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if(transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!"); 
             Destroy(gameObject); 
         }
 
