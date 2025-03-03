860e3be22069e52de08975349dcf59fae0f11b6b
Rudy Gutierrez <93001008+rudy-gm@users.noreply.github.com>
2025-03-02 20:54:29 -0500

    Lesson 4.4 - For-Loops For Waves

--- Projects/Prototype4/Assets/Scripts/SpawnManager.cs
+++ Projects/Prototype4/Assets/Scripts/SpawnManager.cs
@@ -4,20 +4,32 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject enemyPrefab;
-    private float spawnRange = 9; 
+    private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab; 
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);     
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length; 
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation); 
+        }
     }
 
     private Vector3 GenerateSpawnPosition()
@@ -28,5 +40,13 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos; 
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i =0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
