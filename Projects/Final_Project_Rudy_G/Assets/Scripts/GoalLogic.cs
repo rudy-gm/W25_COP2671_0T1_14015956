349c4ac07c1b97467e181b0b4d73229577804d4c
Rudy Gutierrez <93001008+rudy-gm@users.noreply.github.com>
2025-04-24 22:09:12 -0400

    tweak particles effect

--- Projects/Final_Project_Rudy_G/Assets/Scripts/GoalLogic.cs
+++ Projects/Final_Project_Rudy_G/Assets/Scripts/GoalLogic.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class GoalLogic : MonoBehaviour
 {
@@ -6,6 +7,7 @@ public class GoalLogic : MonoBehaviour
     public AudioSource audioSource; 
     public AudioClip goalSound;
     public ParticleSystem goalParticles; 
+    public float particleDuration = 2f; // Duration for the particle system to play
 
     private void OnTriggerEnter(Collider other){
         
@@ -14,9 +16,16 @@ public class GoalLogic : MonoBehaviour
             audioSource.PlayOneShot(goalSound);
             goalParticles.Play();
 
-            FindObjectOfType<GameManager>().OnGoalScored(); 
+            StartCoroutine(StopParticlesAfterDelay());
+
+            FindFirstObjectByType<GameManager>().OnGoalScored(); 
         }
 
+
+    }
+    private IEnumerator StopParticlesAfterDelay(){
+        yield return new WaitForSeconds(particleDuration); // Wait for the specified duration
+        goalParticles.Stop(); // Stop the particle system
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
